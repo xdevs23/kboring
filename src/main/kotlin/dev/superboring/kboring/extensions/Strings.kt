30c946316435563a5bdeb1b0781d356158ad1690
Simão Gomes Viana <devel@superboring.dev>
2022-06-10 00:47:54 +0200

    extensions: Strings: add base64 val

--- src/main/kotlin/dev/superboring/kboring/extensions/Strings.kt
+++ src/main/kotlin/dev/superboring/kboring/extensions/Strings.kt
@@ -1,8 +1,11 @@
 package dev.superboring.kboring.extensions
 
+import java.util.*
+
 internal infix fun Int.repeat(seq: String): String =
     (0 until this).fold("") { acc, _ -> "$acc$seq" }
 
 public val Iterable<String>.linesAsString: String get() = joinToString("\n")
 public operator fun String.times(count: Int): String = count repeat this
 
+public val String.base64: String get() = Base64.getEncoder().encodeToString(toByteArray())
