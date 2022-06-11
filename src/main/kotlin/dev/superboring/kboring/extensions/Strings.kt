b090dc0682967f2033fe902f85aa5ca17eb2c5a0
Simão Gomes Viana <devel@superboring.dev>
2022-06-11 22:44:55 +0200

    Replace StandardCharsets with just Charsets

--- src/main/kotlin/dev/superboring/kboring/extensions/Strings.kt
+++ src/main/kotlin/dev/superboring/kboring/extensions/Strings.kt
@@ -1,7 +1,6 @@
 package dev.superboring.kboring.extensions
 
 import java.net.URLDecoder
-import java.nio.charset.StandardCharsets
 import java.util.*
 
 internal infix fun Int.repeat(seq: String): String =
@@ -11,4 +10,4 @@ public val Iterable<String>.linesAsString: String get() = joinToString("\n")
 public operator fun String.times(count: Int): String = count repeat this
 
 public val String.base64: String get() = Base64.getEncoder().encodeToString(toByteArray())
-public val String.urlDecoded: String get() = URLDecoder.decode(this, StandardCharsets.UTF_8)
+public val String.urlDecoded: String get() = URLDecoder.decode(this, Charsets.UTF_8)
